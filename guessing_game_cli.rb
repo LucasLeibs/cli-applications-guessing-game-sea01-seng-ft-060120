0441987702a7312523b7cd8e587f7008999469ec
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-21 16:44:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,25 @@
+def generate_num
+  rand(6) + 1
+end
+
 # Code your solution here!
+def prompt_user
+  puts "Guess a number"
+   gets.chomp
+end
+
+
+
+def run_guessing_game
+generate_num
+prompt_user
+
+if prompt_user == generate_num
+  puts "You guessed the correct number!"
+elsif prompt_user == "exit" 
+  puts "Goodbye!"
+else 
+  "Sorry! The computer guessed #{generate_num}"
+end
+end
+
