d9e5b00535e7907775125cb0242fbaba673920fd
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-21 17:16:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,6 +4,7 @@ def run_guessing_game
 puts "Guess a number between 1 and 6"
 number = rand(6) + 1
 input = gets.chomp
+
 if input == number.to_s
   puts "You guessed the correct number!"
 elsif input == "exit" 
